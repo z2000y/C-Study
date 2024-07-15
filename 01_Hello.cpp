800e7f788ade734a6803d85ddfe53223f0772842
z2000y <101537306+z2000y@users.noreply.github.com>
2024-07-15 16:02:25 +0800

    Add files via upload

---
+++ 01_Hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//单行注释
+/*
+多行注释
+*/
+int main() 
+{
+	cout << "Hello C++" << endl;//输出Hello C++
+	system("pause");
+	return 0;
+}
+/*
+main是一个程序的入口
+每个程序有且仅有一个
+*/
