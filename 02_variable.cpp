a0ad4bf0caadfd8f3c6b462cbf61e3c30aad0d55
z2000y <101537306+z2000y@users.noreply.github.com>
2024-07-15 16:03:32 +0800

    Add files via upload

---
+++ 02_variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+变量存在的意义：方便我们管理内存空间
+变量创建的语法： 数据类型 变量名=变量初始值;
+               例子：int a=10;
+*/
+
+int main()
+{
+	int a = 10;
+
+	cout << "a=" << a << endl;
+
+	system("pause");
+	return 0;
+}
